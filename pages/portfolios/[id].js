a053a6f79d31737362baef1815ae31d24c4ee7f6
sheng yeong <chia.shengyeong@gmail.com>
2022-08-01 17:32:28 +0800

    adding post details in show portfolio page

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,11 +1,27 @@
 import BaseLayout from "../../components/layouts/BaseLayout";
+import { useRouter } from "next/router";
+import axios from "axios";
 
-const PortfolioDetail = () =>{
+const Portfolio = ({portfolio}) =>{
   return (
     <BaseLayout>
-      <h1>I am portfolios</h1>
+      <p>{portfolio.id}</p>
+      <p>{portfolio.title}</p>
+      <p>{portfolio.body}</p>
     </BaseLayout>
   )
 }
 
-export default PortfolioDetail;
+Portfolio.getInitialProps = async ({query}) => {
+  let post = {};
+  try {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+    post = res.data;
+  } catch(e) {
+    console.error(e);
+  }
+
+  return { portfolio: post };
+}
+
+export default Portfolio;
