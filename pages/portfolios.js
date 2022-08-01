66dd5b7a5a9dde280668938fb13688b9b92787b4
sheng yeong <chia.shengyeong@gmail.com>
2022-08-01 17:03:54 +0800

    add axios & get post data

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,34 @@
 import BaseLayout from "../components/layouts/BaseLayout";
+import axios from "axios";
 
-const Portfolios = () => {
+
+const Portfolios = ({posts}) => {
+  const renderPosts = () => {
+    return posts.map(
+      post => <li key={post.id}>{post.id}</li>
+    )
+  }
+
   return (
     <BaseLayout>
       <h1>I am Portfolios page</h1>
+      <ul>
+        {renderPosts()}
+      </ul>
     </ BaseLayout>
   )
+}
+
+Portfolios.getInitialProps = async () => {
+  let posts = [];
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    posts = res.data;
+  } catch(e) {
+    console.error(e);
+  }
+
+  return { posts: posts.slice(0, 10)};
 }
 
 export default Portfolios;
