c4bc9658141dc993c7790f8c2c90dcaab3412772
sheng yeong <chia.shengyeong@gmail.com>
2022-08-02 10:11:44 +0800

    adding header
    take a look at the navbartoggle, it is a good way to open and close navbars

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,11 +1,16 @@
-import Header from "../shared/Header";
+import Header from '../shared/Header';
 
 const BaseLayout = props => {
+  const { className, children } = props;
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {props.children}
-    </>
+      <main className={`cover ${className}`}>
+        <div className="wrapper">
+          {children}
+        </div>
+      </main>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,54 @@
-import BaseLayout from "../components/layouts/BaseLayout";
-import { Container, Button } from "reactstrap";
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import { Container, Row, Col } from 'reactstrap';
 
-const Home = () => {
-  return (
-    <BaseLayout>
-      <h1>I am Index page</h1>
-      <div className="parent-class sibling-class">
-        <h1>Hi there student!</h1>
-        <h2>Hi there student 2!</h2>
-        <div className="parent-class-title">
-          Hello World
+class Index extends React.Component {
+  render() {
+    return (
+      <BaseLayout className="cover">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper`}>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png"/>
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Filip Jerga.
+                    Get informed, collaborate and discover projects I was working on through the years!
+                  </h1>
+                </div>
+                <div className="hero-welcome-bio">
+                  <h1>
+                    Let's take a look on my work.
+                  </h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
-        <div className="parent-class-date">
-          2000
-        </div>
-      </div>      
-    </ BaseLayout>
-  )
+      </BaseLayout>
+    )
+  }
 }
 
-export default Home;
+export default Index;
