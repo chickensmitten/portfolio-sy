a3bf83bf55d1988268501d6c3891c9861d8dd529
sheng yeong <chia.shengyeong@gmail.com>
2022-08-02 09:25:51 +0800

    adding scss with explanations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 reactstrap and bootstrap version
 
 npm install --save reactstrap@8.9.0 bootstrap@4.4.1
+
+if using scss, have to npm install --save sass
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,16 @@ const Home = () => {
   return (
     <BaseLayout>
       <h1>I am Index page</h1>
+      <div className="parent-class sibling-class">
+        <h1>Hi there student!</h1>
+        <h2>Hi there student 2!</h2>
+        <div className="parent-class-title">
+          Hello World
+        </div>
+        <div className="parent-class-date">
+          2000
+        </div>
+      </div>      
     </ BaseLayout>
   )
 }
