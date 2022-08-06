aad7c883447cfec7b4aea9d2d6ee065c3538dd67
sheng yeong <chia.shengyeong@gmail.com>
2022-08-06 13:45:51 +0800

    handling errors in forms and also added edit page

---
+++ pages/api/v1/portfolios/[id].js
@@ -0,0 +1,6 @@
+import PortfolioApi from '@/lib/api/portfolios';
+
+export default async function handlePortfolio(req, res) {
+  const json = await new PortfolioApi().getById(req.query.id);
+  return res.json(json.data);
+}
