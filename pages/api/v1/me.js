fc0cc62186bb8a8b263e11f3756bfffcf9d1c2a5
sheng yeong <chia.shengyeong@gmail.com>
2022-08-07 09:55:01 +0800

    added edit and delete button

--- pages/api/v1/me.js
+++ pages/api/v1/me.js
@@ -4,7 +4,6 @@ export default async function me(req, res) {
   try {
     await auth0.handleProfile(req, res);
   } catch(error) {
-    console.error(error);
     res.status(error.status || 400).end(error.message);
   }
 }
