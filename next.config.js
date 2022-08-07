9d1ccf7e082b2988d8ffa5a9c34379ad1ce60cd4
sheng yeong <chia.shengyeong@gmail.com>
2022-08-07 10:44:14 +0800

    previously as using old next. new next handles paths and also auto get .env files without needing dotenv webpack

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
-
-
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
 module.exports = {
-  webpack: config => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    config.plugins.push(new Dotenv({silent: true}));
-    return config;
-  },
   env: {
     AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
   }  
