fcb6390ceb2eca3f4dd0aa7565b25966255bdd4f
sheng yeong <chia.shengyeong@gmail.com>
2022-08-08 15:04:28 +0800

    refactor with baseapi class extending from another class

--- pages/api/v1/portfolios/index.js
+++ pages/api/v1/portfolios/index.js
@@ -1,7 +1,7 @@
 import PortfolioApi from '@/lib/api/portfolios';
 import auth0 from '@/utils/auth0';
 
-export default async function createPortfolio(req, res) {
+export default async function create(req, res) {
   try {
     const { accessToken } = await auth0.getSession(req);
     const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
