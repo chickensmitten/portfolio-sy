252d280cbe6b1c1d870d7fe69cbc1ef63a19bd57
sheng yeong <chia.shengyeong@gmail.com>
2022-08-08 15:20:17 +0800

    added dashboard

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -69,7 +69,7 @@ const AdminMenu = () => {
           <DropdownItem>
             <BsNavLink
               className="port-dropdown-item"
-              href="/blogs/dashboard"
+              href="/dashboard"
               title="Dashboard"
             />
           </DropdownItem>
