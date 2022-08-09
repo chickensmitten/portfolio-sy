2c5c6b748a56be3dde936748f4020b5bb8eb5055
sheng yeong <chia.shengyeong@gmail.com>
2022-08-09 09:42:57 +0800

    adding pdf

--- pages/cv.js
+++ pages/cv.js
@@ -1,13 +1,18 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import { useGetUser } from '@/actions/user';
+import { Row, Col } from 'reactstrap';
 
 const CV = () => {
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
       <BasePage>
-        <h1>I am cv page</h1>
+        <Row>
+          <Col md={{size: 8, offset: 2}}>
+            <iframe style={{width: '100%', height: '800px'}} src="/followers.pdf"/>
+          </Col>
+        </Row>
       </BasePage>
     </ BaseLayout>
   )
